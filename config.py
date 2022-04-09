6684684d53ec6df96085ec6ad1691d0b0fc1a0a2
maulanairfanf <maulanairfanf@gmail.com>
2022-04-09 21:22:52 +0700

    change url database

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:TanpaPassword@localhost:5000/webScraping'
+DATABASE_URL = 'postgresql://sgipiulsrqjqyd:c03109765d8a9574c3adc9273c6e07f531997fbfe0722e1c40e93d8184b0b53f@ec2-34-231-63-30.compute-1.amazonaws.com:5432/durspbi4nbo1m'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
