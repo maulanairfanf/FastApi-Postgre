a4c1bf89f587504d196b4733fb200862eb6013d2
maulanairfanf <maulanairfanf@gmail.com>
2022-04-11 21:01:11 +0700

    fix filter

--- crud.py
+++ crud.py
@@ -6,13 +6,13 @@ def get_all(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Berita).offset(skip).limit(limit).all()
 
 def filter_title(db: Session, title: str):
-    return db.query(Berita).filter(Berita.title == title).all()
+    return db.query(Berita.title).filter(Berita.title == title).all()
 
 def filter_date(db: Session, date: str):
-    return db.query(Berita).filter(Berita.date == date).all()
+    return db.query(Berita.date).filter(Berita.date == date).all()
 
 def filter_category(db: Session, category: str):
-    return db.query(Berita).filter(Berita.category == category).all()
+    return db.query(Berita.category).filter(Berita.category == category).all()
 
 def filter_website(db: Session, website: str):
-    return db.query(Berita).filter(Berita.website == website).all()
+    return db.query(Berita.website).filter(Berita.website == website).all()
