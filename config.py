ed856cd55faecbc4900c4163590e1a724388e459
maulanairfanf <maulanairfanf@gmail.com>
2022-04-11 21:13:54 +0700

    fix ffilter

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = 'postgresql://sgipiulsrqjqyd:c03109765d8a9574c3adc9273c6e07f531997fbfe0722e1c40e93d8184b0b53f@ec2-34-231-63-30.compute-1.amazonaws.com:5432/durspbi4nbo1m'
+# DATABASE_URL = 'postgresql://postgres:TanpaPassword@localhost:5000/webScraping'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
--- crud.py
+++ crud.py
@@ -5,14 +5,14 @@ from models import Berita
 def get_all(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Berita).offset(skip).limit(limit).all()
 
-def filter_title(db: Session, title: str):
-    return db.query(Berita.title).filter(Berita.title == title).all()
+def get_title(db: Session, title: str):
+    return db.query(Berita).filter(Berita.title == title).all()
 
-def filter_date(db: Session, date: str):
-    return db.query(Berita.date).filter(Berita.date == date).all()
+def get_date(db: Session, date: str):
+    return db.query(Berita).filter(Berita.date == date).all()
 
-def filter_category(db: Session, category: str):
-    return db.query(Berita.category).filter(Berita.category == category).all()
+def get_category(db: Session, category: str):
+    return db.query(Berita).filter(Berita.category == category).all()
 
-def filter_website(db: Session, website: str):
-    return db.query(Berita.website).filter(Berita.website == website).all()
+def get_website(db: Session, website: str):
+    return db.query(Berita).filter(Berita.website == website).all()
