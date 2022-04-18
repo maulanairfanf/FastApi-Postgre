b24b140d158c1cdcb0fd6cf8e92e1bc71e9b0a90
maulanairfanf <maulanairfanf@gmail.com>
2022-04-18 21:37:29 +0700

    typo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgres: // vrsvsluhxmvrnh: 1be4aa85913309c31a26df4842d9763f7dd4dca289aad373fea555a3542d499f@ec2-34-192-210-139.compute-1.amazonaws.com: 5432/deqif9hrnf9p2t'
+DATABASE_URL = 'postgresql://vrsvsluhxmvrnh:1be4aa85913309c31a26df4842d9763f7dd4dca289aad373fea555a3542d499f@ec2-34-192-210-139.compute-1.amazonaws.com:5432/deqif9hrnf9p2t'
 # DATABASE_URL = 'postgresql://postgres:TanpaPassword@localhost:5000/webScraping'
 
 engine = create_engine(DATABASE_URL)
