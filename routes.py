0c9998a155bbf406948ad0e4ad744ee9aaa2ee2e
maulanairfanf <maulanairfanf@gmail.com>
2022-04-18 22:00:19 +0700

    clear data

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter
 from fastapi import Depends
 from config import SessionLocal
 from sqlalchemy.orm import Session
@@ -22,30 +22,32 @@ async def get_all(db: Session = Depends(get_db)):
     _berita = crud.get_all(db)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
+
 @router.get("/title/{title}")
-async def filter_title(title:str,db:Session = Depends(get_db)):
-    _berita = crud.get_title(db,title)
+async def filter_title(title: str, db: Session = Depends(get_db)):
+    _berita = crud.get_title(db, title)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
 
 @router.get("/date/{date}")
-async def filter_date(date:str,db:Session = Depends(get_db)):
-    _berita = crud.get_date(db,date)
+async def filter_date(date: str, db: Session = Depends(get_db)):
+    _berita = crud.get_date(db, date)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
 
 @router.get("/website/{website}")
-async def filter_website(website:str,db:Session = Depends(get_db)):
-    _berita = crud.get_website(db,website)
+async def filter_website(website: str, db: Session = Depends(get_db)):
+    _berita = crud.get_website(db, website)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
 
 @router.get("/category/{category}")
-async def filter_category(category:str,db:Session = Depends(get_db)):
-    _berita = crud.get_category(db,category)
+async def filter_category(category: str, db: Session = Depends(get_db)):
+    _berita = crud.get_category(db, category)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
 
 @router.get("/author/{author}")
-async def filter_author(author:str,db:Session = Depends(get_db)):
-    _berita = crud.get_author(db,author)
+async def filter_author(author: str, db: Session = Depends(get_db)):
+    _berita = crud.get_author(db, author)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-
-
