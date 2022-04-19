40990006b97769d33d079e7e3d1fd8aa79f716f1
maulanairfanf <maulanairfanf@gmail.com>
2022-04-19 22:33:17 +0700

    edit website filter

--- crud.py
+++ crud.py
@@ -1,4 +1,3 @@
-# from sqlalchemy import literal
 from sqlalchemy.orm import Session
 from models import Berita
 
@@ -6,17 +5,22 @@ from models import Berita
 def get_all(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Berita).offset(skip).limit(limit).all()
 
+
 def get_title(db: Session, title: str):
     return db.query(Berita).filter(Berita.title.contains(title.capitalize())).all()
+
 
 def get_date(db: Session, date: str):
     return db.query(Berita).filter(Berita.date == date).all()
 
+
 def get_category(db: Session, category: str):
     return db.query(Berita).filter(Berita.category == category).all()
 
-def get_website(db: Session, website: str):
-    return db.query(Berita).filter(Berita.website == website).all()
 
-def get_author(db: Session, author:str) :
+def get_website(db: Session, website: str):
+    return db.query(Berita).filter(Berita.website.contains(website)).all()
+
+
+def get_author(db: Session, author: str):
     return db.query(Berita).filter(Berita.author.contains(author.capitalize())).all()
