776c771bc8404c33723402ce93d0d40f16f383c6
maulanairfanf <maulanairfanf@gmail.com>
2022-04-22 23:10:10 +0700

    add more query

--- crud.py
+++ crud.py
@@ -2,6 +2,19 @@ from sqlalchemy.orm import Session
 from models import Berita
 
 
+def merge_query(table, column, params):
+    if(params == "title"):
+        return table.title.contains(column.capitalize())
+    if(params == "author"):
+        return table.author.contains(column.capitalize())
+    if(params == "website"):
+        return table.website.contains(column.lower())
+    if(params == "date"):
+        return table.date == column
+    if(params == "category"):
+        return table.category.contains(column.lower())
+
+
 def query_title(table, column):
     return table.title.contains(column.capitalize())
 
@@ -42,7 +55,10 @@ def get_category(db: Session, category: str):
 
 
 def get_website(db: Session, website: str):
-    return db.query(Berita).filter(query_website(Berita, website)).all()
+    try:
+        return db.query(Berita).filter(query_website(Berita, website)).all()
+    except:
+        return db
 
 
 def get_author(db: Session, author: str):
@@ -167,5 +183,43 @@ def get_website_author_limit(db: Session, website: str, author: str, limit: int)
 # 2 query category limit
 
 
-def get_category_author_limit(db: Session, category: str, author: str, limit: int):
-    return db.query(Berita).filter(query_category(Berita, category), query_author(Berita, author)).limit(limit).all()
+def body_request(db: Session, title: str, date: str, website: str, category: str, author: str):
+    _count = 0
+    arr_params = []
+    arr_data_params = []
+    if title is not None:
+        arr_params.append("title")
+        arr_data_params.append(title)
+        _count = _count + 1
+
+    if date is not None:
+        arr_params.append("date")
+        arr_data_params.append(date)
+        _count = _count + 1
+
+    if website is not None:
+        arr_params.append("website")
+        arr_data_params.append(website)
+        _count = _count + 1
+
+    if category is not None:
+        arr_params.append("category")
+        arr_data_params.append(category)
+        _count = _count + 1
+
+    if author is not None:
+        arr_params.append("author")
+        arr_data_params.append(author)
+        _count = _count + 1
+
+    if(_count == 2):
+        return db.query(Berita).filter(merge_query(Berita, arr_data_params[0], arr_params[0]), merge_query(Berita, arr_data_params[1], arr_params[1])).all()
+
+    if(_count == 3):
+        return db.query(Berita).filter(merge_query(Berita, arr_data_params[0], arr_params[0]), merge_query(Berita, arr_data_params[1], arr_params[1]), merge_query(Berita, arr_data_params[2], arr_params[2])).all()
+
+    if(_count == 4):
+        return db.query(Berita).filter(merge_query(Berita, arr_data_params[0], arr_params[0]), merge_query(Berita, arr_data_params[1], arr_params[1]), merge_query(Berita, arr_data_params[2], arr_params[2]), merge_query(Berita, arr_data_params[3], arr_params[3])).all()
+
+    if(_count == 5):
+        return db.query(Berita).filter(merge_query(Berita, arr_data_params[0], arr_params[0]), merge_query(Berita, arr_data_params[1], arr_params[1]), merge_query(Berita, arr_data_params[2], arr_params[2]), merge_query(Berita, arr_data_params[3], arr_params[3]), merge_query(Berita, arr_data_params[4], arr_params[4])).all()
--- routes.py
+++ routes.py
@@ -1,10 +1,16 @@
+from unicodedata import category
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from config import SessionLocal
 from sqlalchemy.orm import Session
 from schemas import Response
+from typing import Optional
 import crud
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -50,175 +56,181 @@ async def filter_author(author: str, db: Session = Depends(get_db)):
     _berita = crud.get_author(db, author)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
-# params 2 title
+# # params 2 title
 
 
-@router.get("/title={title}/date={date}")
-async def filter_title_date(title: str, date: str, db: Session = Depends(get_db)):
-    _berita = crud.get_title_date(db, title, date)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/date={date}")
+# async def filter_title_date(title: str, date: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_date(db, title, date)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/title={title}/website={website}")
-async def filter_title_website(title: str, website: str, db: Session = Depends(get_db)):
-    _berita = crud.get_title_website(db, title, website)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/website={website}")
+# async def filter_title_website(title: str, website: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_website(db, title, website)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/title={title}/category={category}")
-async def filter_title_category(title: str, category: str, db: Session = Depends(get_db)):
-    _berita = crud.get_title_category(db, title, category)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/category={category}")
+# async def filter_title_category(title: str, category: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_category(db, title, category)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/title={title}/author={author}")
-async def filter_title_author(title: str, author: str, db: Session = Depends(get_db)):
-    _berita = crud.get_title_author(db, title, author)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/author={author}")
+# async def filter_title_author(title: str, author: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_author(db, title, author)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
-# params 2 date
-
-
-@router.get("/date={date}/website={website}")
-async def filter_date_date(date: str, website: str, db: Session = Depends(get_db)):
-    _berita = crud.get_date_website(db, date, website)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# # params 2 date
 
 
-@router.get("/date={date}/category={category}")
-async def filter_date_category(date: str, category: str, db: Session = Depends(get_db)):
-    _berita = crud.get_date_category(db, date, category)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/date={date}/website={website}")
+# async def filter_date_date(date: str, website: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_website(db, date, website)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/date={date}/author={author}")
-async def filter_date_author(date: str, author: str, db: Session = Depends(get_db)):
-    _berita = crud.get_date_author(db, date, author)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-# params 2 website
+# @router.get("/date={date}/category={category}")
+# async def filter_date_category(date: str, category: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_category(db, date, category)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/website={website}/category={category}")
-async def filter_website_category(website: str, category: str, db: Session = Depends(get_db)):
-    _berita = crud.get_website_category(db, website, category)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/date={date}/author={author}")
+# async def filter_date_author(date: str, author: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_author(db, date, author)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+# # params 2 website
 
 
-@router.get("/website={website}/author={author}")
-async def filter_website_author(website: str, author: str, db: Session = Depends(get_db)):
-    _berita = crud.get_website_author(db, website, author)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-# params 2 category
-
-@router.get("/category={category}/author={author}")
-async def filter_category_author(category: str, author: str, db: Session = Depends(get_db)):
-    _berita = crud.get_category_author(db, category, author)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-# single limit
+# @router.get("/website={website}/category={category}")
+# async def filter_website_category(website: str, category: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_website_category(db, website, category)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/limit={limit}")
-async def get_all_limit(limit: int, db: Session = Depends(get_db)):
-    _berita = crud.get_all_limit(db, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/website={website}/author={author}")
+# async def filter_website_author(website: str, author: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_website_author(db, website, author)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+# # params 2 category
 
 
-@router.get("/title={title}/limit={limit}")
-async def filter_title_limit(title: str, limit: int, db: Session = Depends(get_db)):
-    _berita = crud.get_title_limit(db, title, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/category={category}/author={author}")
+# async def filter_category_author(category: str, author: str, db: Session = Depends(get_db)):
+#     _berita = crud.get_category_author(db, category, author)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+# # single limit
 
 
-@router.get("/date={date}/limit={limit}")
-async def filter_date_limit(date: str, limit: int, db: Session = Depends(get_db)):
-    _berita = crud.get_date_limit(db, date, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/limit={limit}")
+# async def get_all_limit(limit: int, db: Session = Depends(get_db)):
+#     _berita = crud.get_all_limit(db, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/website={website}/limit={limit}")
-async def filter_website_limit(website: str, limit: int, db: Session = Depends(get_db)):
-    _berita = crud.get_website_limit(db, website, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/limit={limit}")
+# async def filter_title_limit(title: str, limit: int, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_limit(db, title, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/category={category}/limit={limit}")
-async def filter_category_limit(category: str, limit: int, db: Session = Depends(get_db)):
-    _berita = crud.get_category_limit(db, category, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/date={date}/limit={limit}")
+# async def filter_date_limit(date: str, limit: int, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_limit(db, date, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/author={author}/limit={limit}")
-async def filter_author_limit(author: str, limit: int, db: Session = Depends(get_db)):
-    _berita = crud.get_author_limit(db, author, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-# 2 params title limit
+# @router.get("/website={website}/limit={limit}")
+# async def filter_website_limit(website: str, limit: int, db: Session = Depends(get_db)):
+#     _berita = crud.get_website_limit(db, website, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/title={title}/date={date}/limit={limit}")
-async def filter_title_date_limit(title: str, date: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_title_date_limit(db, title, date, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/category={category}/limit={limit}")
+# async def filter_category_limit(category: str, limit: int, db: Session = Depends(get_db)):
+#     _berita = crud.get_category_limit(db, category, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/title={title}/website={website}/limit={limit}")
-async def filter_title_website_limit(title: str, website: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_title_website_limit(db, title, website, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/author={author}/limit={limit}")
+# async def filter_author_limit(author: str, limit: int, db: Session = Depends(get_db)):
+#     _berita = crud.get_author_limit(db, author, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+# # 2 params title limit
 
 
-@router.get("/title={title}/category={category}/limit={limit}")
-async def filter_title_category_limit(title: str, category: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_title_category_limit(db, title, category, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/date={date}/limit={limit}")
+# async def filter_title_date_limit(title: str, date: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_date_limit(db, title, date, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/title={title}/author={author}/limit={limit}")
-async def filter_title_author_limit(title: str, author: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_title_author_limit(db, title, author, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-# 2 params date limit
+# @router.get("/title={title}/website={website}/limit={limit}")
+# async def filter_title_website_limit(title: str, website: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_website_limit(db, title, website, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/date={date}/website={website}/limit={limit}")
-async def filter_date_website_limit(date: str, website: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_date_website_limit(db, date, website, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/category={category}/limit={limit}")
+# async def filter_title_category_limit(title: str, category: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_category_limit(db, title, category, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/date={date}/category={category}/limit={limit}")
-async def filter_date_category_limit(date: str, category: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_date_category_limit(db, date, category, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/title={title}/author={author}/limit={limit}")
+# async def filter_title_author_limit(title: str, author: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_title_author_limit(db, title, author, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+# # 2 params date limit
 
 
-@router.get("/date={date}/author={author}/limit={limit}")
-async def filter_date_author_limit(date: str, author: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_date_author_limit(db, date, author, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
-
-# 2 params website limit
+# @router.get("/date={date}/website={website}/limit={limit}")
+# async def filter_date_website_limit(date: str, website: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_website_limit(db, date, website, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/website={website}/category={category}/limit={limit}")
-async def filter_website_category_limit(website: str, category: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_website_category_limit(db, website, category, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/date={date}/category={category}/limit={limit}")
+# async def filter_date_category_limit(date: str, category: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_category_limit(db, date, category, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
 
-@router.get("/website={website}/author={author}/limit={limit}")
-async def filter_website_author_limit(website: str, author: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_website_author_limit(db, website, author, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+# @router.get("/date={date}/author={author}/limit={limit}")
+# async def filter_date_author_limit(date: str, author: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_date_author_limit(db, date, author, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
 
-# 2 parasm category limit
+# # 2 params website limit
 
 
-@router.get("/category={category}/author={author}/limit={limit}")
-async def filter_category_author_limit(category: str, author: str, limit=int, db: Session = Depends(get_db)):
-    _berita = crud.get_category_author_limit(db, category, author, limit)
+# @router.get("/website={website}/category={category}/limit={limit}")
+# async def filter_website_category_limit(website: str, category: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_website_category_limit(db, website, category, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+
+# @router.get("/website={website}/author={author}/limit={limit}")
+# async def filter_website_author_limit(website: str, author: str, limit=int, db: Session = Depends(get_db)):
+#     _berita = crud.get_website_author_limit(db, website, author, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+# # 2 parasm category limit
+
+
+# @router.get("/category={category}/author={author}/limit={limit}")
+# async def filter_category_author_limit(category: str, author: str, limit=int, ):
+#     _berita = crud.get_category_author_limit(db, category, author, limit)
+#     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
+
+@router.get("/body_requests")
+async def body_request(db: Session = Depends(get_db), title: Optional[str] = None, date: Optional[str] = None, website: Optional[str] = None, category: Optional[str] = None, author: Optional[str] = None):
+    _berita = crud.body_request(db, title, date, website, category, author)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_berita)
