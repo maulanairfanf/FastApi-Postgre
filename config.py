ecc0b2008c67ae9528c615dc86ef62d13a9e8ed6
maulanairfanf <maulanairfanf@gmail.com>
2022-05-13 16:31:09 +0700

    connect hostinger

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# mysql+pymysql//u116665791_maulanairfanf:TanpaPassword79@sql596.main-hosting.eu/u116665791_WebScraping
 
-DATABASE_URL = 'postgresql://lcwsaujkplbtoa:f222a0c158038fbc55d1b815143ab41e42f3ae27f9d8d999ef7c944e4490cda6@ec2-34-197-84-74.compute-1.amazonaws.com:5432/d93p694gn91dn4'
+DATABASE_URL = 'mysql+pymysql://u116665791_maulanairfanf:TanpaPassword79@31.220.110.101:3306/u116665791_WebScraping'
 # DATABASE_URL = 'postgresql://postgres:TanpaPassword@localhost:5432/webScraping'
 
 engine = create_engine(DATABASE_URL)
